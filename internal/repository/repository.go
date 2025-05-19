413f8e59ccc9d689a56ecae76c02b1eb2cead9d7
Alexei <lexa.morit@gmail.com.>
2025-05-19 12:37:04 +0500

    Switch

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -7,3 +7,15 @@ var (
 	headphones []*model.Headphones
 	tvs        []*model.TV
 )
+
+switch v (type) := model. {
+	
+case *model.Laptop:
+	model.Laptop = append(model.Laptop,v)
+
+case *model.Headphones:
+	*model.Headphones = append(*model.Headphones,v)	
+
+case *model.TV = append(*model.TV,v)	
+	
+}
